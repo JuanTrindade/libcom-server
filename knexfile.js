189471c42e32954e978f3af81b4cf8dd58a2e1af
Pedro Ramos <pedroramos.contato@outlook.com>
2022-08-12 21:59:23 -0300

    Fix migration config in knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -4,11 +4,12 @@ const path = require('path');
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-
   development: {
     client: 'pg',
     connection: process.env.PG_CONNECTION_STRING,
-    migrations: path.resolve(__dirname, 'src', 'database', 'migrations')
+    migrations: {
+      directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
+      name: 'knex_migrations',
+    },
   },
-
 };
