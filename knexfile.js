d5abfd3ac1fbb3d3b60cef4a9b72dbbfea7aa957
Pedro Ramos <pedroramos.contato@outlook.com>
2022-08-18 21:54:47 -0300

    Update knexfile
    Adds a testing configuration for the knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,9 @@
 const path = require('path');
-require('dotenv').config();
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'testing'
+    ? '.env.testing'
+    : '.env'
+});
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
@@ -13,4 +17,13 @@ module.exports = {
       name: 'knex_migrations',
     },
   },
+
+  testing: {
+    client: 'pg',
+    connection: process.env.PG_CONNECTION_STRING,
+    migrations: {
+      directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
+      name: 'knex_migrations',
+    },
+  }
 };
