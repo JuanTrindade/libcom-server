7f5a4d1907ccd3273b33e84a38437e460feb945f
Pedro Ramos <pedroramos.contato@outlook.com>
2022-09-01 20:16:36 -0300

    Add profile service test base

---
+++ test/unit/services/profile.service.test.js
@@ -0,0 +1,9 @@
+const profileService = require('../../../src/services/profile.service');
+
+describe('Testing profile service', () => {
+  describe('.create', () => {
+    it.todo('registers a profile if it does not exists');
+
+    it.todo('does not register two profiles with the same username');
+  });
+});
