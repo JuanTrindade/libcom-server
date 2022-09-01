7acc193ab0b101d77cfa1ee28bdf08351b0fd9e3
Pedro Ramos <pedroramos.contato@outlook.com>
2022-09-01 20:31:51 -0300

    Add first profile test

--- test/unit/services/profile.service.test.js
+++ test/unit/services/profile.service.test.js
@@ -2,7 +2,18 @@ const profileService = require('../../../src/services/profile.service');
 
 describe('Testing profile service', () => {
   describe('.create', () => {
-    it.todo('registers a profile if it does not exists');
+    it('registers a profile if it does not exists', async () => {
+      // prepare the test
+      const dummyProfile = { name: 'Test', username: 'test', password: '123' };
+
+      // execute subject
+      const subject = await profileService.create(dummyProfile);
+
+      // asserts
+      expect(subject).not.toBeNull();
+      expect(subject.name).toBe('Test');
+      expect(subject.username).toBe('test');
+    });
 
     it.todo('does not register two profiles with the same username');
   });
