ae4616e363bb0d0dadd694a6fa15013cefc768df
Juan <juantrindade123@gmail.com>
2022-09-26 12:03:21 -0300

    fix unhandled profile creation

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -4,10 +4,15 @@ exports.create = async (req, res) => {
   // TODO: encrypt password
   const { name, username, password, avatar_url } = req.body;
 
-  // TODO: check if profile already exists. If it does, throw an error.
-  const profile = await profileService.create({
-    name, username, password, avatar_url
-  });
-
-  return res.status(201).send(profile);
+  try {
+    const profile = await profileService.create({
+      name, username, password, avatar_url
+    });
+    
+    return res.status(201).send(profile);
+  } catch (error) {
+    return res.status(500).send({
+      message: error.message
+    });
+  }
 };
